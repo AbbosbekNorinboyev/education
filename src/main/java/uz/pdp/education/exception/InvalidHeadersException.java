e4845d795cb179afbc86b52cac66af9c76a5100d
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2025-09-09 10:31:55 +0500

    fix

---
+++ src/main/java/uz/pdp/education/exception/InvalidHeadersException.java
@@ -0,0 +1,11 @@
+package uz.pdp.education.exception;
+
+public class InvalidHeadersException extends RuntimeException {
+    public InvalidHeadersException(String message) {
+        super(message);
+    }
+
+    public InvalidHeadersException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
